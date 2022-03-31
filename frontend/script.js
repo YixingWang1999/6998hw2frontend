c57c51e98d880d37a135ace4204bb022020384bb
YixingWang1999 <84959521+YixingWang1999@users.noreply.github.com>
2022-03-31 00:47:52 -0400

    first attempt

---
+++ frontend/script.js
@@ -0,0 +1,31 @@
+const image_input = document.querySelector("#image_input");
+const label_input = document.querySelector("#fname");
+var labels = document.getElementById("fname").value;  
+
+
+
+image_input.addEventListener("change", function(){
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        uploaded_image = reader.result;
+        document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`
+    });
+    reader.readAsDataURL(this.files[0]);
+    var labels = document.getElementById("fname").value;  
+    console.log(labels);
+    var x = this.files[0]['name']
+    fetch(' https://nu0bv58t6b.execute-api.us-east-1.amazonaws.com/test1/upload/yw3747-b2/'+x,{
+        method:'PUT',
+        headers:{
+            'Content-Type': this.files[0].type,
+          
+            "x-amz-meta-customLabels": labels        
+        },
+        body: this.files[0]
+    }) 
+    
+})
+
+
+
+
